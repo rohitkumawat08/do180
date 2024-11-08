a120bdcb228ee1ec4aac49dc8f0f0dfeee0a9c5b
Rohit Kumawat <123355745+rohitkumawat08@users.noreply.github.com>
2024-11-08 14:05:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My first PHP page version my Webhooks </h1>
+<h1> Hello Guys this is rohit </h1>
 
 <?php
 echo "hello World from test FROM S2I 2nd time VERSION 3.2  LEARNING OPENSHIFT 4.14";
